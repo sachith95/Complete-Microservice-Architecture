ec1844479a47e30d32d807df05c48b208830e03f
sachith silva <sachithsilva2012@gmail.com>
2021-07-25 23:37:19 +0530

    kafka admin config

--- app-config-data/src/main/java/com/microservices/one/config/RetryConfigData.java
+++ app-config-data/src/main/java/com/microservices/one/config/RetryConfigData.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
-@ConfigurationProperties
+@ConfigurationProperties(prefix = "retry-config")
 public class RetryConfigData {
     private Long initialIntervalMs;
     private Long maxIntervalMs;
--- kafka/kafka-admin/src/main/java/com/microservices/one/kafka/admin/config/client/KafkaAdminClient.java
+++ kafka/kafka-admin/src/main/java/com/microservices/one/kafka/admin/config/client/KafkaAdminClient.java
@@ -1,2 +1,31 @@
-package com.microservices.one.kafka.admin.config.client;public class KafkaAdminClient {
+package com.microservices.one.kafka.admin.config.client;
+
+import com.microservices.one.config.KafkaConfigData;
+import com.microservices.one.config.RetryConfigData;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.support.RetryTemplate;
+
+public class KafkaAdminClient {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaAdminClient.class);
+
+    private KafkaConfigData kafkaConfigData;
+
+    private RetryConfigData retryConfigData;
+
+    private AdminClient adminClient;
+
+    private RetryTemplate retryTemplate;
+
+    public KafkaAdminClient(KafkaConfigData config,
+                            RetryConfigData retryConfigData,
+                            AdminClient client,
+                            RetryTemplate template) {
+        this.kafkaConfigData = config;
+        this.retryConfigData = retryConfigData;
+        this.adminClient = client;
+        this.retryTemplate = template;
+    }
 }
